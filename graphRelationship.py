b412642b447821486b38641289be6a492df91994
Nicholas Bardy <nicholasbardy@yahoo.com>
2012-02-09 00:22:57 -0500

    updated stuff

--- graphRelationship.py
+++ graphRelationship.py
@@ -2,24 +2,37 @@ import sys
 import getopt
 import scrapePurple
 
-def diagnose(line):
-   words = line.split(' ')
-   if len(words) == 0
+def diagnose(string):
+   words = string.split(' ')
+   if len(words) == 0:
       return None
-   if len(words) == 1
-      return {'before':None, 'word':words[0], 'after':None}
-   for index in range(1,len(words)):
-      if 
+   if len(words) == 1:
+      return [{'word': words[1]}]
+
+   wordmap = []
+   for index in range(len(words)): 
+      if index == 0:
+         wordmap.append({'word': words[index], 'after': words[index+1]})
+      elif index == len(words) - 1:
+         wordmap.append({'before': words[index-1], 'word': words[index]})
+      else :
+         wordmap.append({'before': words[index-1], 'word': words[index], 'after': words[index+1]})
+
+   return wordmap
 
 def main(argv=None):
    if argv is None:
       argv = sys.argv
+   
+   return diagnose(sys.argv[1])
 
-   linelist = scrapePurple.main(sys.argv[1])
-   datamap = {}
+def processFile(file):
+
+   linelist = scrapePurple.main(file)
+   datastore = {}
 
    for line in linelist:
       wordmap = diagnose(line)
-
+      addWordMap(datastore, wordmap)
 if __name__ == "__main__":
     sys.exit(main())
