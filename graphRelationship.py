eae03fb0562368bff99155441f7d604f22887c25
Nicholas Bardy <nicholasbardy@yahoo.com>
2012-02-09 02:24:08 -0500

    Working

--- graphRelationship.py
+++ graphRelationship.py
@@ -7,14 +7,14 @@ def diagnose(string):
    if len(words) == 0:
       return None
    if len(words) == 1:
-      return [{'word': words[1]}]
+      return [{'before': None, 'word': words[0], 'after': None}]
 
    wordmap = []
    for index in range(len(words)): 
       if index == 0:
-         wordmap.append({'word': words[index], 'after': words[index+1]})
+         wordmap.append({'before': None, 'word': words[index], 'after': words[index+1]})
       elif index == len(words) - 1:
-         wordmap.append({'before': words[index-1], 'word': words[index]})
+         wordmap.append({'before': words[index-1], 'word': words[index], 'after': None})
       else :
          wordmap.append({'before': words[index-1], 'word': words[index], 'after': words[index+1]})
 
@@ -22,17 +22,50 @@ def diagnose(string):
 
 def main(argv=None):
    if argv is None:
-      argv = sys.argv
+      argv = sys.argv[1]
    
-   return diagnose(sys.argv[1])
+   return processFile(argv)
 
-def processFile(file):
+def addWordMaps(datastore, wordmaps):
+   for wordmap in wordmaps:
+      word = wordmap['word']
+      if word in datastore:
+         updateWordMap(datastore, wordmap)
+      else:
+         beforeword = wordmap['before']
+         afterword = wordmap['after']
+         datastore[word]={'count': 1, 'before':{beforeword: 1}, 'after':{afterword: 1}}
 
-   linelist = scrapePurple.main(file)
+def updateWordMap(datastore, wordmap):
+   word = wordmap['word'] 
+
+   datastore[word]['count'] += 1
+
+   beforeword = wordmap['before']
+   afterword = wordmap['after']
+
+   beforemap = datastore[word]['before']
+   aftermap = datastore[word]['after']
+
+   if afterword in aftermap:
+      aftermap[afterword] += 1
+   else :
+      aftermap[afterword] = 1
+
+   if beforeword in beforemap:
+      beforemap[beforeword] += 1
+   else :
+      beforemap[beforeword] = 1
+
+def processFile(filedir):
+   linelist = scrapePurple.main(filedir)
    datastore = {}
 
    for line in linelist:
-      wordmap = diagnose(line)
-      addWordMap(datastore, wordmap)
+      wordmaps = diagnose(line)
+      addWordMaps(datastore, wordmaps)
+
+   return datastore
+
 if __name__ == "__main__":
     sys.exit(main())
--- scrapePurple.py
+++ scrapePurple.py
@@ -16,17 +16,19 @@ def main(argv=None):
    datalist = []
 
    for filename in dirList:
-         datalist += scrapedata(open(directory + filename).read()) 
+         datalist += scrapedata(directory + filename) 
    
    return datalist
 
-def scrapedata(file):
+
+def scrapedata(filedir):
    """
    Scarpes data from given file
    Returns data as a list of each line
    """
+   fstring = open(filedir).read()
    expr = ':</b></font>\s([\s\w]*)<br/>'
-   return re.findall(expr, file)
+   return re.findall(expr, fstring)
 
 if __name__ == "__main__":
    sys.exit(main())
